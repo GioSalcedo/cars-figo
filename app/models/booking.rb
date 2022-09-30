b2f3ec4997c29334755e8af08fed3231af321b96
Giovanny Salcedo <gsalcedo26@gmail.com>
2022-09-30 14:24:45 -0500

    Algunas rutas

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,6 @@
 class Booking < ApplicationRecord
   belongs_to :users
   belongs_to :vehicles
+  validates_presence_of :start_date, :end_date, :user_id, :vehicle_id
+  validates_uniqueness_of :vehicle_id, scope: [:start_date, :end_date]
 end
