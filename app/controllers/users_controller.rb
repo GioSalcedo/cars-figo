a8a1b2533517e0f23ecb94414616e87159c596ad
Giovanny Salcedo <gsalcedo26@gmail.com>
2022-10-06 19:21:59 -0500

    Cambios rutas y show y views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,8 @@ class UsersController < ApplicationController
 
   def login
   end
+
+  def show
+    @user = User.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   resources :vehicles, only: [:index, :show] do
-    resources :bookings, only: [:index, :show, :new, :create, :update, :edit, :destroy]
+    resources :bookings, only: [:new, :create]
+  end
+
+  resources :users, only: [:show] do
+    resources :bookings, only: %i[index show]
   end
 end
